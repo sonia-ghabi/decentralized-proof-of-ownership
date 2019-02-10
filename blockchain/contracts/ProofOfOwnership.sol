ec3c147f50385e1008e30d756d60b5d2685fdd07
Sonia Ghabi <sonia.ghabi@gmail.com>
2019-02-10 18:43:57 +0800

    Smart contracts

---
+++ blockchain/contracts/ProofOfOwnership.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.0;
+
+contract ProofOfOwnership {
+
+    // Definition of a proof of ownership.
+    struct proof { 
+        string hashing;
+        address owner; 
+        uint date;  
+        bool isValue;
+    }
+
+    // The map that stores all the proof of ownership.
+    mapping (string => proof) proofMap;
+
+    /**
+        Save a new proof of ownership.
+    */
+    function saveProof(string memory hashing, address owner) public  {
+        if (proofMap[hashing].isValue)
+        {
+            revert("The P.O.O. already exist");
+        }
+        proofMap[hashing] = proof(hashing, owner, now, true);
+    } 
+
+    /**
+        Get a proof of ownership from its hashing.
+     */
+    function getProof(string memory hashing) public view returns (address, uint) {
+        if (!proofMap[hashing].isValue)
+        {
+            revert("The P.O.O. doesn't exist");
+        } 
+        proof memory p = proofMap[hashing];
+        return (p.owner, p.date);
+    } 
+}
