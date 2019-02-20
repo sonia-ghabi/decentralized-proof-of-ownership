337f013b64d0f1a0b0f5f93f6c5a83e30864513f
Sonia Ghabi <sonia.ghabi@gmail.com>
2019-02-20 22:17:44 +0800

    Add watermark on saved images

--- src/lib/proofOfOwnerShip.js
+++ src/lib/proofOfOwnerShip.js
@@ -5,6 +5,9 @@ const Hashing = require("./hashing.js");
 const uuidv1 = require("uuid/v1");
 const crypto = require("crypto");
 const eccrypto = require("eccrypto");
+const watermark = require("image-watermark");
+const sharp = require("sharp");
+const path = require("path");
 
 // Set provider
 // Once a Ganache node is running, it behaves very similar to a
@@ -23,21 +26,19 @@ const contract = new Contract(contractAddress, ProofContract.abi, wallet);
  * @param {string} owner
  */
 async function saveProof(buffer, owner, userPublicKey) {
-  // Save the file on IPFS
+  // Save the watermarked file on IPFS
+  const watermarked = sharp(buffer).overlayWith(
+    path.join(__basedir, "../watermark.jpeg")
+  );
+  const ipfsHash = await ipfs.saveFile(watermarked);
+
+  // Encrypt the file and save it on IPFS
   const guid = uuidv1();
   const encryptedBuffer = Hashing.encryptBuffer(buffer, guid);
-  const encryptedKey = await eccrypto.encrypt(userPublicKey, Buffer.from(guid));
-  /*
-  console.log(encryptedKey);
-  console.log(encryptedKey.toString());
-  const decrypted = await eccrypto.decrypt(
-    Hashing.decryptBuffer(userPublicKey.private),
-    encryptedKey
-  );
-  console.log(decrypted.toString("ascii"));
-  */
-  const ipfsHash = await ipfs.saveFile(buffer);
   const ipfsHashEncrypted = await ipfs.saveFile(encryptedBuffer);
+
+  // Encrypt the encryption key with the user public key
+  const encryptedKey = await eccrypto.encrypt(userPublicKey, Buffer.from(guid));
 
   // Hash the document
   const hash = Hashing.getHash(buffer);
@@ -45,10 +46,10 @@ async function saveProof(buffer, owner, userPublicKey) {
   // Save the proof in the blockchain
   const results = await contract.saveProof(hash, owner, ipfsHash);
 
-  // Return the IPFS in the results
-  results.ipfsHash = ipfsHash;
+  // Return the IPFS hashs in the results
   results.ipfsHashEncrypted = ipfsHashEncrypted;
   results.encryptedKey = encryptedKey;
+  results.ipfsHash = ipfsHash;
   return results;
 }
 
